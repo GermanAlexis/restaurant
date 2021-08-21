7ca67279d37a2a6614a5f84fb18c76dea01579db
German Alexis <gaalvarez@saludelectronica.com>
2021-08-21 18:37:11 -0500

    Express validator config

--- polymathBack/app/routes/restaurant.js
+++ polymathBack/app/routes/restaurant.js
@@ -7,6 +7,7 @@ const {
   updateRestuarant,
   deleteRestuarant,
 } = require('../controllers/restaurant');
+const { validateCampus } = require('../helpers/field-valid');
 
 
 const router = Router();
@@ -17,18 +18,23 @@ router.get('/:id', getByIdRestaurant);
 router.post(
   '/',
   [
-    check('name_medic', 'el nombre es obligatorio').not().isEmpty(),
-    check('hospital', 'el id del Hospital debe ser valido').isMongoId(),
+    check('name', 'el nombre es obligatorio').not().isEmpty(),
+    check('description', 'la descripcion es necesaria').not().isEmpty(),
+    check('address', 'la address es necesaria').not().isEmpty(),
+    check('city', 'la cuidad es necesaria').not().isEmpty(),
+    check('url_img', 'la url_img es necesaria').not().isEmpty(),
+    validateCampus
   ],
   createRestuarant
 );
 router.put(
   '/:id',
   [
-    check('name_medic', 'el nombre es obligatorio').not().isEmpty(),
-    check('area_medic', 'el area es obligatorio').not().isEmpty(),
-    check('hospital', 'El Id del Hospital debe ser valido').isMongoId(),
-
+    check('name', 'el nombre es obligatorio').not().isEmpty(),
+    check('description', 'la descripcion es necesaria').not().isEmpty(),
+    check('address', 'la address es necesaria').not().isEmpty(),
+    check('url_img', 'la url_img es necesaria').not().isEmpty(),
+    validateCampus
   ],
   updateRestuarant
 );
